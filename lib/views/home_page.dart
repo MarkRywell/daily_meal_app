dc80800d1a4afabdc8ef1cf7e7b5b99e2b20045d
Mark Rywell Gaje <gopro855@gmail.com>
2023-01-10 14:42:29 +0800

    Updated UI

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:daily_meal_app/models/query_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomePage extends StatefulWidget {
 
@@ -10,18 +13,114 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  Future fetchMeals() {
+  List mealsList = [];
+
+  checkPermissions() async {
+    var status = await Permission.camera.status;
+
+    if(status.)
 
   }
 
-
-
   @override
   Widget build(BuildContext context) {
+
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: FutureBuilder(
+        future: QueryBuilder.instance.mealDays(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: const Icon(Icons.error_outline,
+                          size: 100,
+                          color: Colors.redAccent
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Text("Database Error: Problem Fetching Data",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            height: 1.5,
+                            fontSize: 20
+                        ),),
+                    )
+                  ],
+                ),
+              );
+            }
+            if (snapshot.hasData) {
 
-      )
+              if (snapshot.data!.isEmpty) {
+                return Center(
+                  child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Container(
+                            height: size.height * 0.2,
+                            child: Lottie.asset('assets/foods.json'),
+                          ),
+
+                          const Text("You got no meals",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500
+                              ))
+                        ],
+                      )
+                  ),
+                );
+              }
+              else {
+                mealsList.isEmpty ? mealsList = snapshot.data! : null;
+
+                return NestedScrollView(
+                  floatHeaderSlivers: true,
+                    headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                      SliverAppBar(
+                        title: Text("Daily Meal"),
+                        centerTitle: true,
+                        backgroundColor: Colors.greenAccent,
+                      )
+                    ],
+                    body: ListView.builder(
+                        itemCount: mealsList.length,
+                        itemBuilder: (context, index) {
+
+                          final meal = mealsList[index];
+
+                          return ListTile(
+                            title: meal.date,
+                          );
+                        }
+                    )
+                );
+
+              }
+            }
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+        },
+        child: Icon(Icons.fastfood_outlined),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
