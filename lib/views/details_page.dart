757948b0879ae784eacb64318b8667e27413c5bd
Mark Rywell Gaje <gopro855@gmail.com>
2023-01-12 19:21:25 +0800

    Working details page

--- lib/views/details_page.dart
+++ lib/views/details_page.dart
@@ -27,72 +27,68 @@ class _MealDetailsState extends State<MealDetails> {
       appBar: AppBar(
         backgroundColor: Colors.greenAccent.withOpacity(0.4),
         elevation: 0,
+        title: Text(widget.meal.date,
+            style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black54
+            )),
+        centerTitle: true,
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
           },
           icon: Icon(Icons.arrow_back_ios,
-          color: Colors.grey[700],),
+            color: Colors.grey[700],),
         ),
       ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text(widget.meal.date,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),),
-              ),
-              ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: widget.foods.length,
-                  itemBuilder: (context, index) {
+      body: ListView.builder(
+          shrinkWrap: true,
+          itemCount: widget.foods.length,
+          itemBuilder: (context, index) {
 
-                    final food = widget.foods[index][1];
+            final food = widget.foods[index][1];
 
-                    return Container(
-                        margin: const EdgeInsets.all(20),
-                        width: size.width * 0.8,
-                        height: size.height * 0.5,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.red
-                        ),
-                        child: Column(
-                          children: [
-                            Container(
-                              margin: const EdgeInsets.all(10),
-                              width: size.width * 0.8,
-                              height: size.height * 0.38,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  image: DecorationImage(
-                                  image: FileImage(File(food.photo)),
-                                  fit: BoxFit.fitWidth
-                    )
-                              ),
-                            ),
-                            Text(food.name,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24
-                            ),),
-                            Text(widget.foods[index][0],
-                            style: const TextStyle(
-                              fontSize: 16
-                            ),)
-                          ],
-                        )
-                    );
-                  })
-
-
-            ],
-          ),
-      )
+            return Container(
+                margin: const EdgeInsets.all(20),
+                width: size.width * 0.8,
+                height: size.height * 0.5,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.lightGreen[100],
+                    border: Border.all(color: Colors.black12)
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      width: size.width * 0.8,
+                      height: size.height * 0.38,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: FileImage(File(food.photo)),
+                              fit: BoxFit.fill
+                          )
+                      ),
+                    ),
+                    Text(food.name,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24
+                        )),
+                    Text(widget.foods[index][0],
+                        style: const TextStyle(
+                            fontSize: 16
+                        )),
+                    Text(food.foodType,
+                      style: const TextStyle(
+                          fontSize: 12
+                      ),)
+                  ],
+                )
+            );
+          }),
     );
   }
 }
